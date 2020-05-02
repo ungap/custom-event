a04cd976dad9a751eac9051309706ab7e28ddee8
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-05-02 14:55:39 +0200

    made it dualmodule

--- min.js
+++ min.js
@@ -1,2 +1,2 @@
 /*! (c) Andrea Giammarchi - ISC */
-var self=this||{};self.CustomEvent="function"==typeof CustomEvent?CustomEvent:function(t){function e(t,e){e||(e={});var n=document.createEvent("CustomEvent");return n.initCustomEvent(t,!!e.bubbles,!!e.cancelable,e.detail),n}return e.prototype=new e("").constructor.prototype,e}();
+var self=this||{};self.CustomEvent="function"==typeof CustomEvent?CustomEvent:function(t){return e[t]=new e("").constructor[t],e;function e(t,e){e=e||{};var n=document.createEvent("CustomEvent");return n.initCustomEvent(t,!!e.bubbles,!!e.cancelable,e.detail),n}}("prototype");
